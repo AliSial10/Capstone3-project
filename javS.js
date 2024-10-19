4265e26a55b837bd2334e6422df509541a525859
AliSial10 <xwavealisial@gmail.com>
2024-10-19 11:36:17 -0700

    login and register page

---
+++ javS.js
@@ -0,0 +1,34 @@
+let bgColor = document.getElementById("btn")
+bgColor.style.backgroundColor= "#5FA232";
+
+// form sign up
+function data(){
+
+var a = document.getElementById("n1").value;
+var b = document.getElementById("n2").value;
+var c = document.getElementById("n3").value;
+var d = document.getElementById("n4").value;
+
+if (a == "" || b == "" || c == "" || dd == "")
+{
+    alert("Fill the Form Completely!");
+    return false;
+}
+
+
+else if(c!=d)
+
+{
+    alert("Please enter same password");
+    return false
+}
+
+
+
+
+
+
+else{
+     true;
+}
+}
